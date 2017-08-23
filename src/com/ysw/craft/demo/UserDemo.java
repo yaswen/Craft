e55ae8bf1aa571d279f11864e3ef7b042d9c4d31
yaswen <yaswen@163.com>
2017-08-23 23:12:02 +0800

    给第一个demo增加注释

--- src/com/ysw/craft/demo/UserDemo.java
+++ src/com/ysw/craft/demo/UserDemo.java
@@ -2,13 +2,13 @@ package com.ysw.craft.demo;
 
 import com.ysw.craft.dao.UserDao;
 import com.ysw.craft.pojos.User;
-
+/*意义重大的代码，诗文第一个实际demo*/
 public class UserDemo {
 	public static void main(String[] args){
-		User user=UserDao.getUserByUserName("shiwen");
-		int i=user.getGold();
-		i+=10;
-		user.setGold(i);
-		UserDao.updateUser(user);
+		User user=UserDao.getUserByUserName("shiwen");//使用Dao类中的查询方法，获得名叫shiwen的User对象
+		int i=user.getGold();//获得shiwen的金钱
+		i+=10;//给金钱数目加10
+		user.setGold(i);//将变量存入对象
+		UserDao.updateUser(user);//更新对象持久化
 	}
 }
