c50961d173ded246e6b2970d6eea4710a023fdd4
yaswen <yaswen@163.com>
2017-09-04 23:30:57 +0800

    增加聊天功能（v0.1）

---
+++ WebContent/chat.jsp
@@ -0,0 +1,17 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>微波炉</title>
+</head>
+<body>
+	<div><h3>欢迎光临微波炉聊天机！</h3></div>
+	<div id="chattext"></div>
+	<form action="chat" method="post">
+		<input type="text" name="text"/><br/>
+		<input type="submit" value="发送"/>
+	</form>
+</body>
+</html>
---
+++ src/com/ysw/craft/action/ChatAction.java
@@ -0,0 +1,54 @@
+package com.ysw.craft.action;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import org.apache.struts2.interceptor.ServletRequestAware;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class ChatAction extends ActionSupport implements ServletRequestAware{
+		private String text;
+		
+	
+		
+		public String getText() {
+			return text;
+		}
+		public void setText(String text) {
+			this.text = text;
+		}
+		//声明request对象
+		private HttpServletRequest request;
+		//重写ServletRequestAware中的方法
+		public void setServletRequest(HttpServletRequest request){
+			this.request=request;
+		}
+		public String execute(){
+			System.out.println("开始聊天action");
+			String t=text;
+			System.out.println(t + "代表着："+text);
+			//获得session对象
+			HttpSession session=request.getSession();
+			System.out.println("成功获取session");
+			if(null!=session.getAttribute("ChatText")){
+				List<String> list=(List<String>) session.getAttribute("ChatText");
+			}else{
+				List<String> list=new ArrayList();
+				list.add("聊天记录：");
+				session.setAttribute("ChatText", list);
+			}
+			List<String> list=(List<String>) session.getAttribute("ChatText");
+			System.out.println("成功获取list"/*+list.get(0)*/);
+			if(list.size()==0 || list==null){
+				list.add("聊天记录：");
+			}
+			list.add(t);
+			//将聊天内容保存到session中
+			session.setAttribute("ChatText", list);
+			return "success";
+		}
+}
