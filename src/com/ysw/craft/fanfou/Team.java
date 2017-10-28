5b63ab9b407f230546ffc1bbcfb79562b2251c64
yaswen <yaswen@163.com>
2017-10-29 00:08:32 +0800

    成功取到球员的信息！之前是分隔符搞错了，换行应为\r\n

--- src/com/ysw/craft/fanfou/Team.java
+++ src/com/ysw/craft/fanfou/Team.java
@@ -5,11 +5,16 @@ import com.ysw.craft.demo.txttest;
 public class Team {
 	
 	public static void main(String[] args) {
-		getTeams("雷霆");
+		Team[] t=getTeam();
+		String name=t[0].players[1].getName();
+		int a=t[0].players[1].getA();
+		int p=t[0].players[1].getP();
+		int d=t[0].players[1].getD();
+		System.out.println("球员姓名："+name+"\t基础进攻："+a+"\t转换进攻："+p+"\t防守："+d);
 	}
 	public String name;
 	public Player[] players;
-	int lineUp[][];
+	public int[][] lineUp;
 	
 	public int[][] getLineUp() {
 		return lineUp;
@@ -40,8 +45,37 @@ public class Team {
 		String t1=txttest.basketballfile().split("&")[3];
 		String p1=t1.split("\n")[3];
 		String pn1=p1.split(" ")[0];
-		System.out.println(p1);
+		System.out.println(pn1);
 		return team;
-		
+	}
+	public static Team[] getTeam() {
+		String txt=txttest.basketballfile();
+		System.out.println(txt);
+		Team[] teams=new Team[5];
+		for(int i=1;i<=teams.length;i++) {
+			String tx=txt.split("&")[i];//第0个舍去
+			System.out.println(tx);
+			String tname=tx.split("\n")[0];//第一个的位置为队名
+			System.out.println(tname);
+			teams[i-1]=new Team();
+			//teams[i-1].setName(tname);//获取队名
+			System.out.println(teams[i-1].name);
+			Player[] players=new Player[10];//声明Players数组
+			for(int j=1;j<=10;j++) {
+				String t=tx.split("\r\n")[j];
+				players[j-1]=new Player();
+				players[j-1].setName(t.split(" ")[0]);//0位置为球员名
+				players[j-1].setA(Integer.valueOf(t.split(" ")[1]));//1位置为基础攻击
+				players[j-1].setP(Integer.valueOf(t.split(" ")[2]));//2位置为转换攻击
+				players[j-1].setD(Integer.valueOf(t.split(" ")[3]));//3位置为防守
+			}
+			
+			teams[i-1].setPlayers(players);
+			int[][] lineUp=new int[12][5];//声明lineUp数组
+			
+			
+			teams[i-1].setLineUp(lineUp);
+		}
+		return teams;
 	}
 }
